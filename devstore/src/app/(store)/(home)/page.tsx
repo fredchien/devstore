19bcc32c5a97039f109215ca08a9af124fc9c0aa
Frederic Chien <fred_chien@yahoo.com.br>
2024-09-26 11:07:49 -0300

    feat: add skeleton and loading home page

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,63 +1,75 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/products";
 import Image from "next/image";
 import Link from "next/link";
 
+async function getFeaturedProducts(): Promise<Product[]>{
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60,
+    }
+  });
+
+  const products = await response.json();
+
+  return products;
+}
+
 export default async function Home() {
+  const [highlightedProducts, ...otherProducts] = await getFeaturedProducts();
+  
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-row-6 gap-6">
-      <Link href="/" className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
+      <Link href={`/product/${highlightedProducts.slug}`} className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
         <Image 
-          src="/moletom-never-stop-learning.png"
+          src={highlightedProducts.image}
           className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
           quality={100}
-          alt="Moletom Never Stop Learning"
+          alt={highlightedProducts.title}
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="text-sm truncate">{highlightedProducts.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {highlightedProducts.price.toLocaleString('pt-BR', { 
+              style: 'currency', 
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0
+            })}
           </span>
         </div>   
       </Link>
 
-      <Link href="/" className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
-        <Image 
-          src="/moletom-java.png"
-          className="group-hover:scale-105 transition-transform duration-500"
-          width={920}
-          height={920}
-          quality={100}
-          alt="Moletom Never Stop Learning"
-        />
+      {otherProducts.map((product) => {
+        return (
+          <Link key={product.id} href={`/product/${product.slug}`} className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
+            <Image 
+              src={product.image}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={920}
+              height={920}
+              quality={100}
+              alt={product.title}
+            />
 
-        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div> 
-      </Link>
-
-      <Link href="/" className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
-        <Image 
-          src="/camiseta-dowhile-2022.png"
-          className="group-hover:scale-105 transition-transform duration-500"
-          width={920}
-          height={920}
-          quality={100}
-          alt="Moletom Never Stop Learning"
-        />
-
-        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div> 
-      </Link>
-
+            <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {product.price.toLocaleString('pt-BR', { 
+                style: 'currency', 
+                currency: 'BRL',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0
+              })}
+              </span>
+            </div> 
+          </Link>
+        );
+      })}
     </div>
   );
 }
